4251b5a7510701f142c6c56ce5194ba973c69a5c
LUIS <luis.enrique.enc@gmail.com>
2022-12-07 22:42:49 -0600

    [ADD] = SE AGREGA FUNCIONES EN BOTON DE DISPARA, Y REINICIAR JUEGO

--- GAMEAPP/ViewController.swift
+++ GAMEAPP/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var lblMainTitle: UILabel!{
         didSet{ self.lblMainTitle.layer.cornerRadius = 8 }
     }
@@ -27,20 +28,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblPuntuacion: UILabel!
     @IBOutlet weak var lblMarcadorPuntuacion: UILabel!
     @IBOutlet weak var lblRonda: UILabel!
-    @IBOutlet weak var lblNoRonda: UIImageView!
+    
+    @IBOutlet weak var lblNoRonda: UILabel!
+    
     @IBOutlet weak var btnSiguienteRonda: UIButton!{
         didSet{ self.btnSiguienteRonda.layer.cornerRadius = 8 }
     }
     // V A R I A B L E S
 
     var randomValue : Int = 0
-    var rondas = 0
+    var rondas: Int = 0
+    var pointNumber: Int = 0
+    var sliderValue: Int = 0
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.randomValue = self.generateRandomValue()
         self.lblValorAleatorio.text = String(randomValue)
+        self.setUpSlider()
     }
     
     func generateRandomValue () -> Int {
@@ -48,8 +55,65 @@ class ViewController: UIViewController {
     }
     
     func setUpSlider (){
-        
+        self.sliderTiro.minimumValue = 0
+        self.sliderTiro.maximumValue = 100
+        self.sliderTiro.value = 50
+        self.sliderTiro.setThumbImage(UIImage(named: "SliderThumb-Normal"), for: .normal)
+        self.lblMarcadorPuntuacion.text = "\(pointNumber)"
+        self.lblNoRonda.text = "\(rondas)"
     }
+    
+    @IBAction func getSlideNumber(_ sender: Any) {
+        self.sliderValue = Int(self.sliderTiro.value)
+    }
+    
+    
+    @IBAction func btnDispara(_ sender: Any) {
+        self.valida(valor1: randomValue, valor2: sliderValue)
+        self.lblMarcadorPuntuacion.text = "\(pointNumber)"
+        self.rondas += 1
+        self.lblNoRonda.text = "\(rondas)"
+        self.btnDispara.isHidden = true
+    }
+
+    
+    
+    func valida(valor1 num1: Int, valor2 num2 : Int){
+        let val = abs(num1-num2)
+        if val == 0 {
+            print("ganaste 100 puntos")
+            self.pointNumber += 100
+        } else if val <= 5 {
+            print("Ganaste 10 puntos")
+            self.pointNumber += 10
+        } else if val <= 10 {
+            print("No ganas puntos")
+        } else {
+            print("No pues no compa")
+        }
+    }
+    
+    @IBAction func btnSiguienteRonda(_ sender: Any) {
+        self.randomValue = self.generateRandomValue()
+        self.lblValorAleatorio.text = String(randomValue)
+        self.setUpSlider()
+        self.btnDispara.isHidden = false
+        if lblNoRonda.text == "3" {
+            btnSiguienteRonda.isHidden = true
+        }
+        if lblNoRonda.text == "3" {
+            btnDispara.isHidden = true
+        }
+    }
+    
+    @IBAction func btnReiniciarJuego(_ sender: Any) {
+        self.randomValue = self.generateRandomValue()
+        self.lblValorAleatorio.text = String(randomValue)
+        self.setUpSlider()
+        btnSiguienteRonda.isHidden = false
+        btnDispara.isHidden = false
+    }
+    
 }
 
 
